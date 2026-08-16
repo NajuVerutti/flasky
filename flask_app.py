46d3d364b986e1e8d671ad1e9ec1b1eb88cbec26
NajuVerutti <julia.verutti@aluno.ifsp.edu.br>
2026-08-16 15:54:16 +0000

    Formulario com Flask-WTF

--- flask_app.py
+++ flask_app.py
@@ -3,9 +3,20 @@ from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class NameForm(FlaskForm):
+  name = StringField('What is your name?', validators= [DataRequired()])
+  submit = SubmitField('Submit')
+
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Chave forte'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -15,13 +26,27 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-@app.route("/")
-def home():
+#@app.route("/")
+#def home():
 
-    return render_template(
-        "home.html",
-        current_time=datetime.now()
+#    return render_template(
+#        "home.html",
+#        current_time=datetime.now()
+#    )
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+  name = None
+  form = NameForm()
+  if form.validate_on_submit():
+    name = form.name.data
+    form.name.data = ''
+  return render_template('formulario.html',
+                         form=form,
+                         name=name
     )
+
+
 
 @app.route('/user/<name>/<pront>/<insti>')
 def user(name, pront, insti):
